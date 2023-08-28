a25afeb0759b82e2ebbdf1c211c58f400dcf26cf
ravi barla <ravibarla1@gmail.com>
2023-08-28 20:13:19 +0530

    adding fetch url to get all the contact lists

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Heading from "./component/Heading";
 import ContactList from "./component/ContactsList";
 import AddContact from "./component/AddContact";
+
+async function fetchContact() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((jsonData) => console.log(jsonData));
+}
 function App() {
-  const [heading,setHeading] = useState("My Contact List");
+  const [heading, setHeading] = useState("My Contact List");
+
+  useEffect(() => fetchContact, []);
+
   return (
     <div className="App">
       <Heading heading={heading} />
       <ContactList />
       <AddContact />
+      {/* <button onClick={fetchContact}>view Contact</button> */}
     </div>
   );
 }
