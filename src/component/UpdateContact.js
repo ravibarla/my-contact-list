29bf869252ba34a0f13794b9d8cd2adfa5844a02
ravi barla <ravibarla1@gmail.com>
2023-08-30 20:17:38 +0530

    updating the updateOperation by adding a useEffect

--- src/component/UpdateContact.js
+++ src/component/UpdateContact.js
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const UpdateContact = (props) => {
+  const { editableContactId, handleUpdateContact } = props;
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
-  const { editableContactId, handleUpdateContact } = props;
+  useEffect(() => {
+    setName(editableContactId.name);
+    setPhone(editableContactId.phone);
+  }, [editableContactId]);
   const handleForm = (e) => {
     e.preventDefault();
     handleUpdateContact({ id: editableContactId, name, phone });
   };
   return (
     <form onSubmit={(e) => handleForm(e)}>
-      <input
-        placeholder={editableContactId.name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        placeholder={editableContactId.name}
-        onChange={(e) => setPhone(e.target.value)}
-      />
+      <input placeholder={name} onChange={(e) => setName(e.target.value)} />
+      <input placeholder={phone} onChange={(e) => setPhone(e.target.value)} />
       <button className="bg-secondary">UPDATE</button>
     </form>
   );
